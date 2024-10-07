40fe169ffaa51e3642e09a5df07f9d8df1cd95b5
BrainCellCoder <00abhisheksharma1999@gmail.com>
2024-10-07 09:25:50 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,11 @@ const App = () => {
   return (
     <div id="main">
       <ol key="relativeList">
-        <li key="relativeListItem1">Uncle Ramesh</li>
-        <li key="relativeListItem2">Aunt Sita</li>
-        <li key="relativeListItem3">Cousin Raj</li>
-        <li key="relativeListItem4">Grandparents</li>
-        <li key="relativeListItem5">Uncle Mohan</li>
+        <li id="relativeListItem1">Uncle Ramesh</li>
+        <li id="relativeListItem2">Aunt Sita</li>
+        <li id="relativeListItem3">Cousin Raj</li>
+        <li id="relativeListItem4">Grandparents</li>
+        <li id="relativeListItem5">Uncle Mohan</li>
       </ol>
     </div>
   );
