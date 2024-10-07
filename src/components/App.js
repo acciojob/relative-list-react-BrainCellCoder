33b61e8a0478b54848ac80a041fba441e23fedb7
BrainCellCoder <00abhisheksharma1999@gmail.com>
2024-10-07 09:22:08 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
     <div id="main">
-               {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <ol key="relativeList">
+        <li key="relativeListItem1">Uncle Ramesh</li>
+        <li key="relativeListItem2">Aunt Sita</li>
+        <li key="relativeListItem3">Cousin Raj</li>
+        <li key="relativeListItem4">Grandparents</li>
+        <li key="relativeListItem5">Uncle Mohan</li>
+      </ol>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
